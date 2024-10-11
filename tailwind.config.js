145a55ca5b1342178e842743703d5ceed0074946
Viniciuscm09 <viniciuscm06@gmail.com>
2024-10-10 23:18:54 -0300

    Primeiro commit do projeto de cadastro de usuário

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './build/*.html'
+  ],
+  theme: {
+    extend: {
+
+      colors:{
+        'custom-blue': '#2bc1f9',
+
+
+      },
+    },
+  },
+  plugins: [],
+}
+
